47bfb33487d3e51aa2fc3cc063df60d3d337ed8a
Michael E. Karpeles <mek@baybo.it>
2011-12-04 00:49:12 -0500

    Added SMS and Voice entry points on the srv

--- subapps/v1.py
+++ subapps/v1.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env/python
 
 import web
-
 from apps.api import v1
 
-urls = ( "/sandbox/?", "apps.api.v1.Sandbox",
-         "/?", "apps.api.v1.Api",
-         "/.*", "Error",)
+urls = ("/sandbox/?", "apps.api.v1.Sandbox",
+        "/tincan/sms/?", "apps.api.v1.TincanSMS",
+        "/tincan/voice/?", "apps.api.v1.TincanVoice",
+        "/directions/(.*)/(.*)/?", "apps.api.v1.Directions",
+        "/?", "apps.api.v1.Api",
+        "/.*", "Error",)
 
 #===============================================================#
 # Error
