6d335bd1caf3f57330a95dfea246d3518a195d99
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-15 20:41:25 -0700

    added test placeholder

---
+++ tincan/test/test_tincan.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    test.test_tincan
+    ~~~~~~~~~~~~~~~~
+
+    Test cases for the tincan project
+
+    :copyright: (c) 2012 by Mek
+    :license: BSD, see LICENSE for more details.
+"""
+
+import unittest
+
+class TestTincan(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def testall(self):
+        raise NotImplementedError
+
+    def tearDown(self):
+        pass
